5577bdb0e17f3c7ddec9b83ce74def154ec5bfdd
Fernando Mendes <fernandobvm@gmail.com>
2022-08-12 23:34:53 -0300

    improving teflon properties calculations

--- fdiff/ptfe_properties.m
+++ fdiff/ptfe_properties.m
@@ -1,18 +1,32 @@
-rho_r = 1933;
-rho_mc = 2174;
-T_m = 600;
-T_r = 298;
-
-T_c = [0 298 300 311 317 317.5 322 345 450 554 582 599];
-rho_c = rho_r + (rho_mc - rho_r).*(T_c - T_r)/(T_m-T_r);
-
-rho_h = 1086; 
-rho_ma = 1740; %dont know if its equals to rho_m2
-T_h = 889;
-
-T_a = [601 609 623 689 710 715 769 781 800 900 1000];
-rho_a = rho_h +(rho_h - rho_ma).*(T_a-T_m)/(T_h - T_m);
-
-T = [T_c T_a];
-rho = [rho_c rho_a];
-plot(T,rho)
+function [k, rho, C] = ptfe_properties(T)
+    k_r = 0.2485;
+    k_mc = 0.3619;
+    T_m = 600;
+    T_r = 298;
+    k_h = 0.2472; 
+    k_ma = 0.2453; %dont know if its equals to rho_m2
+    T_h = 889;
+    rho_r = 1933;
+    rho_mc = 2174;
+    rho_h = 1086; 
+    rho_ma = 1740; %dont know if its equals to rho_m2
+    C_r = 708.3;
+    C_mc = 1220;
+    C_h = 1537; 
+    C_ma = 1476; %dont know if its equals to rho_m2
+    
+    %cristaline phase
+    if T < 600 %600K is maximum temperature that ptfe still cristaline
+        k = k_r + (k_mc - k_r).*(T - T_r)/(T_m-T_r);
+        rho = rho_r + (rho_mc - rho_r).*(T - T_r)/(T_m-T_r);
+        C = C_r + (C_mc - C_r).*(T - T_r)/(T_m-T_r);
+    %amorphous phase
+    else
+        k = k_h +(k_h - k_ma).*(T-T_m)/(T_h - T_m);
+        rho = rho_h +(rho_h - rho_ma).*(T-T_m)/(T_h - T_m);
+        C = C_h +(C_h - C_ma).*(T-T_m)/(T_h - T_m);
+    end
+%     k = 0.2477 + 3.1717*10^-4*T;
+%     rho = 1914;
+%     C = 707.9;
+end
---
+++ fdiff/transient_cond_1d_case4v2.m
@@ -0,0 +1,85 @@
+% clear all;
+% close all;
+% clc;
+tic
+m = 400;
+
+L = 0.1;
+dx = L/(m-1);
+% x = 0:dx:L;
+x1 = linspace(3.5*L/5,L,6*m/8+1);
+x2 = linspace(0,3.5*L/5,2*m/8);
+x = [x2(1:end-1), x1];
+
+dx = zeros(1,m-1);
+for i=2:m
+    dx(i-1) = x(i) - x(i-1);
+end
+
+
+% alfa = 0.124e-6;
+% alfa = 9.7e-5;
+% alfa = 1e-4;
+
+% k = 0.3;
+% rho = 2.3e3;
+% d = alfa*dt/dx^2;
+% Fo = d;
+
+
+T0 = 296;
+q = 75900;
+
+tf = 4.1;
+steps = 100;
+% dt = tf/steps;
+%pensar melhor como lidar com o criterio de estabilidade 
+%considerando o alfa variavel
+[k,rho,C] = ptfe_properties(T0);
+alfa0 = 1000*k/(rho*C);
+dt = (min(dx))^2/(2*alfa0);  %criterio de estabildiade - máximo dt
+t = 0:dt/2:tf;
+
+T = ones(m,length(t))*T0;
+% T(1,:) = 0;
+% T(m,:) = 0;
+
+% figure
+for i= 2:length(t)
+    for j=2:m-1
+        [k,rho,C] = ptfe_properties(T(j,i-1));
+        alfa = k/(rho*C);
+        d = alfa*(t(i)-t(i-1))/(x(j)-x(j-1))^2;
+        T(j,i) = T(j,i-1) + d*(T(j-1,i-1) + T(j+1,i-1) - 2*T(j,i-1));
+    end
+%     T(1,i) = T(1,i-1) + d*(T(2,i-1) - 2*T(1,i-1));
+    [k,rho,C] = ptfe_properties(T(m,i-1));
+    alfa = k/(rho*C);
+    d = alfa*(t(i)-t(i-1))/(x(m)-x(m-1))^2;
+    T(m,i) = T(m,i-1) + d*(T(m-1,i-1) - 2*T(m,i-1) + T(m-1,i-1) + 2*(x(m)-x(m-1))*q/(ptfe_conductivity(T(m,i-1))));
+%     plot(x,T(:,i))
+%     title(['t = ', num2str(t(i)), ' s'])
+%     pause(0.05)
+end
+T';
+toc
+result = find(t > 1.9999 & t < 2.0001,1);
+plot(x,T(:,result))
+% T_a = zeros(size(T));
+% x = L - x;
+% for i=2:length(t)
+%         T_a(:,i) = (q./k).*sqrt(4*alfa.*t(i)/pi).*(exp(-x.^2./(4*alfa.*t(i))) - x.*sqrt(pi./(4*alfa.*t(i))).*erfc(x./(sqrt(4*alfa.*t(i))))) + T0;
+% end
+% figure
+% plot(L - x,T_a(:,end))
+% hold on
+% scatter(L - x,T(:,end),'x')
+% xlabel('Comprimento do propelente (m)')
+% ylabel('Temperatura')
+% title('Perfil de Temperatura')
+% legend('Analitic', 'Numerical')
+% 
+% 
+% lT_a = T_a(:,10);
+% lT = T(:,10);
+% save('data/m80_7-8_dt1-10_t=10.mat','lT_a','lT')
